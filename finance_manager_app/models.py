02c8c48457201c61892beef7a0a34f0f82ea406a
alex <martiashviliako2@gmail.com>
2025-11-03 01:25:29 +0400

    Added models

--- finance_manager_app/models.py
+++ finance_manager_app/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model()
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password = None, **extra_fields):
@@ -18,6 +20,7 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault("is_superuser", True)
         return self.create_user(email, password, **extra_fields)
     
+
 class CustomUserModel(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique = True)
     is_staff = models.BooleanField(default=False)
@@ -27,3 +30,28 @@ class CustomUserModel(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
+
+
+class Transaction(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=100)
+    item = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(max_length=8)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class Monthly_budget(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.DecimalField(max_length=100)
+    budget = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(auto_now_add=True)
+
+class Recurring_bill(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    recurring_bill_type = models.CharField(max_length=20)
+    item = models.CharField(max_length=100)
+    transaction_type = models.CharField(max_length=7, default='expense')
+    date = models.DateTimeField(auto_now_add=True)
